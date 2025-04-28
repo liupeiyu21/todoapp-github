c23eb092dce5ab7fbf6b5c457a6470a4d2d3d24a
Liu Peiyu <24aw0142@jec.ac.jp>
2025-04-28 17:25:12 +0900

    完了未完了の実装が完了しました

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ $("#addTodo").click(function () {
     $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
 
     $("input").val("");
+});
+
+$(document).on("change", "input[type='checkbox']", function () {
+    if ($(this).is(":checked")) {
+        $(this).parent().css("text-decoration", "line-through");
+    } else {
+        $(this).parent().css("text-decoration", "none");
+    }
+    
 })
